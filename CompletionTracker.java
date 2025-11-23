399c624d96024a9f311ac01e3e4a0cc9cc5cf30b
Nada Khaled <nadakhaled0357@gmail.com>
2025-11-23 19:06:53 +0200

    Add files via upload

---
+++ CompletionTracker.java
@@ -0,0 +1,19 @@
+package CompletionTracking;
+
+import com.github.tahamostafa06.backend.auth.LoginToken;
+import com.github.tahamostafa06.backend.courseservice.CourseItem;
+
+public class CompletionTracker {
+
+    public boolean isCourseCompleted(LoginToken token, CourseItem courseItem) {
+        var course = courseItem.getCourse();
+        // change type of getCourse to public
+        var doneLessons = course.getStudentsAndLessonsDone().get(token.getUserId());
+
+        if (doneLessons == null) return false;
+
+        int totalLessons = course.getLessons().size();
+        return doneLessons.size() == totalLessons;
+    }
+}
+
